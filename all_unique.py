bfa14265f583cad1796aaf0ef66bd0b37224a1ef
manika15 <manika.kapoor90@gmail.com>
2017-08-14 14:22:37 -0700

    add the comment for time complexity

--- all_unique.py
+++ all_unique.py
@@ -1,3 +1,6 @@
+#The complexity of converting a string to a set is O(n) as time to traverse a string of 'n' characters is O(n) and the time
+#to add it to the hash map is O(1) and the is else loop is again O(1)
+
 def all_unique(s):
     l = set(s)
     if len(s) == len(l):
