82dcaa7f4750f8f141cb85551f98fe0b741edb5b
manika15 <manika.kapoor90@gmail.com>
2017-08-14 22:45:21 -0700

    program to check if permutation of palindrome

---
+++ palindrome_permutation.py
@@ -0,0 +1,26 @@
+"""Given a string, write a function to check if it is a permutation of a palin-drome.
+Check the count of each character. If it is a permutation of a palindrome, only one character can have odd count..
+All others will have even count"""
+
+from collections import Counter
+
+def palin_permutation(s):
+    s = s.lower()
+    d = Counter(s)      #make dict of the characters
+    del d[' ']          #del the key ' '
+    odd_occurred = False
+
+    for value in d.values():
+        if value %2 != 0:               #check if count is odd
+            if odd_occurred == True:    #if already True, it cannot be a permutation of palindrome
+                return False
+            else:
+                odd_occurred = True #set odd_occurred to True if the first occurrence
+    return True
+
+def main():
+    s = input("Enter a string: ")
+    print (palin_permutation(s))
+
+if __name__ == '__main__':
+    main()
