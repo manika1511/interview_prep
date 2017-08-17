b5cdcf941ccbd8b2023468e651df8f3dc2bcfc04
manika15 <manika.kapoor90@gmail.com>
2017-08-17 16:44:37 -0700

    add comments

--- linked_list/node_class.py
+++ linked_list/node_class.py
@@ -5,15 +5,3 @@ class Node(object):
         self.data = data
         self.next_node = next_node
 
-    # method to get the data in a particular node
-    def get_data(self):
-        return self.data
-
-    # method to get the next_node
-    def get_next(self):
-        return self.next_node
-
-    # method to get set the next_node
-    def set_next(self, new_next):
-        self.next_node = new_next
-
