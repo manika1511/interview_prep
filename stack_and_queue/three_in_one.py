6235c55aa285952340464aac9b7632cb4708d708
manika15 <manika.kapoor90@gmail.com>
2017-08-24 16:00:44 -0700

    add method to check if stack is empty

--- stack_and_queue/three_in_one.py
+++ stack_and_queue/three_in_one.py
@@ -3,3 +3,8 @@ class Stack(object):
     #constructor
     def __init__(self):
         self.items=[]
+
+    #method to check if stack is empty
+    def isEmpty(self):
+        return len(self.items)==0
+
