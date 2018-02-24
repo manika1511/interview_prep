e30e462020b939bd70a0cb878d178bb54d606a3c
manika15 <manika.kapoor90@gmail.com>
2018-02-24 15:15:38 -0800

    increment a number by 1

---
+++ array_and_strings/increment_by_1.py
@@ -0,0 +1,18 @@
+def increment(arr):
+    carry = 1 #as one needs to be added
+    sum = 0
+    i = len(arr) - 1
+    while i >= 0:
+        sum = arr[i] + carry
+        carry = int(sum/10)
+        arr[i] = int(sum%10)
+        i = i - 1
+    return arr
+
+def main():
+    arr = [0,8,9,9,9]
+    print (increment(arr))
+
+if __name__ == "__main__":
+    main()
+
