c41ae381615e492586307e64a9cabb281a2a95d0
manika15 <manika.kapoor90@gmail.com>
2018-02-24 01:17:51 -0800

    change the second pointer to start from the end

--- array_and_strings/sorting_subarray_sorts_array.py
+++ array_and_strings/sorting_subarray_sorts_array.py
@@ -8,16 +8,23 @@ def sort_sub(arr):
             start = i
             break
 
-    for i in range(start+1,len(arr)-1):
-        if arr[i] > arr[i+1]:
-            end = i + 1
+    # for i in range(start+1,len(arr)-1):
+    #     if arr[i] > arr[i+1]:
+    #         end = i + 1
+    #         break
+    j = len(arr) - 1
+    while j > start:
+        if arr[j] < arr[j - 1]:
+            end = j
             break
+        j = j - 1
+
     if start == end:
         return "Already sorted"
     return arr[start:end+1]
 
 def main():
-    arr = [1,2,3,4,5,6]
+    arr = [1,2,4,3,2,7,6,8,9]
 
     print(sort_sub(arr))
 
