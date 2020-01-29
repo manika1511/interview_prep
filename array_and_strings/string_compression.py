da168872c446476749432c1d57ad4a1a193c3346
Manika-kapoor <manika.kapoor@ibm.com>
2020-01-28 23:39:39 -0800

    improved solution

--- array_and_strings/string_compression.py
+++ array_and_strings/string_compression.py
@@ -1,26 +1,35 @@
 """Implement a method to perform basic string compression using the counts of repeated characters. Traversing a string 
 wil take O(n) time and all other operations are O(1). So, the entire algorithm has O(n) time complexity. """
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        count = 1
+        write = 0
+        for i in range(len(chars)):
+            if i == len(chars)-1:
+                chars[write] = chars[i]
+                write = write + 1
+                if count > 1:
+                    count = list(str(count))
+                    for ch in count:
+                        chars[write] = ch
+                        write = write + 1
+                return len(chars[0:write])
+            if chars[i] == chars[i+1]:
+                count = count + 1
+            else:
+                chars[write] = chars[i]
+                write = write + 1
+                if count > 1:
+                    count = list(str(count))
+                    for ch in count:
+                        chars[write] = ch
+                        write = write + 1
+                count = 1
 
-def string_compressor(s):
-    output = ""
-    counter = 1
 
-    for i in range(len(s)):
-        if output == "":                #adding the first element in the output
-            output = output + s[i]
-        elif output[-1] != s[i]:        #checking if the last element in output is same as input string current element
-            output = output + str(counter) + s[i] #appending the count to the string and the next element
-            counter = 1
-        else:
-            counter = counter + 1       #increasing the counter if same
 
-    output = output + str(counter)
-    return output
-
-def main():
-    string = input("Enter a string: ")
-    print (string_compressor(string))
-
-if __name__ == '__main__':
-    main()
 
