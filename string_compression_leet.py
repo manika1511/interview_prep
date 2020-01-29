dcbb128e0df94e8c927ff5305739821615b7b43c
Manika-kapoor <manika.kapoor@ibm.com>
2020-01-28 23:38:46 -0800

    add solution for string compression (443)

--- string_compression_leet.py
+++ string_compression_leet.py
@@ -1,31 +1,61 @@
-class Solution(object):
+class Solution1(object): #O(n) time and O(n) space
     def compress(self, chars):
         """
         :type chars: List[str]
         :rtype: int
         """
-        result = []
+        result = ""
         count = 1
         for i in range(len(chars)):
-            if i < len(chars)-1:
-                if chars[i] == chars[i+1]:
-                    count = count + 1
-                else:
-                    result.append(str(chars[i]))
-                    if count > 1:
-                        result.append(str(count))
-                    count = 1
-            else:
-                result.append(str(chars[i]))
+            if i == len(chars)-1:
+                result = result + chars[i]
                 if count > 1:
-                    result.append(str(count))
-        return result
+                    result = result + str(count)
+                return list(result)
+            if chars[i] == chars[i+1]:
+                count = count + 1
+            else:
+                result = result + chars[i]
+                if count > 1:
+                    result = result + str(count)
+                count = 1
+        return list(result)
 
+class Solution(object): #O(n) time and O(1) space
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        count = 1
+        write = 0
+        for i in range(len(chars)):
+            if i == len(chars)-1:
+                chars[write] = chars[i]
+                write = write + 1
+                if count > 1:
+                    count = list(str(count))
+                    for ch in count:
+                        chars[write] = ch
+                        write = write + 1
+                return len(chars[0:write])
+            if chars[i] == chars[i+1]:
+                count = count + 1
+            else:
+                chars[write] = chars[i]
+                write = write + 1
+                if count > 1:
+                    count = list(str(count))
+                    for ch in count:
+                        chars[write] = ch
+                        write = write + 1
+                count = 1
 
 def main():
-    l = ["a","b","c"]
+    s1 = ["a","a","b","b","c","c","c"]
     obj = Solution()
-    print (obj.compress(l))
+
+    print (obj.compress(s1))
 
 
 
